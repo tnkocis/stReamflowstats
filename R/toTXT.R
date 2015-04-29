e2a9846992e7274919994a62f5635d9005fde5e6
Tiffany <tnkocis@users.noreply.github.com>
2015-04-29 11:53:26 -0700

    updates to SVI

---
+++ R/toTXT.R
@@ -0,0 +1,12 @@
+# TODO: Add comment
+# 
+# Author: tiffnk
+###############################################################################
+
+
+# write to text 
+# check modified date of text file 
+format(file.info("//Users//tiffnk//Documents//UCDAVIS//Lab//Streamflow_Analysis//testwrite.txt")$mtime, "%Y-%d-%m")
+# download from dataRetrieval
+# append to text file 
+# rerun stats
