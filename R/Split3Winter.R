f302bc5e798ef9707acde27761a5779289c7e981
Tiffany <tnkocis@users.noreply.github.com>
2015-05-06 17:08:40 -0700

    updates

--- R/Split3Winter.R
+++ R/Split3Winter.R
@@ -14,14 +14,14 @@ Split3Winter <- function(input){
 #
 	if (missing(input))
 		stop("Input data is required.")
-	year <- as.numeric(format(input$raw$Date,"%Y"))
-	month <- as.numeric(format(input$raw$Date,"%m"))
-	day <- as.numeric(format(input$raw$Date,"%d"))
+	year <- as.numeric(format(input$Date,"%Y"))
+	month <- as.numeric(format(input$Date,"%m"))
+	day <- as.numeric(format(input$Date,"%d"))
 	years <- seq(min(year), max(year), by=1)
 	
 	wint_by_year <- list()
 	for (i in seq(1,length(years)-1,1)){
-		wint_by_year[[i]] <- filter(input$raw, (month >11 & year == years[i])| (month <3 & year == years[i]+1))
+		wint_by_year[[i]] <- filter(input, (month >11 & year == years[i])| (month <3 & year == years[i]+1))
 		names(wint_by_year)[i] <- paste(years[i],"-",years[i]+1)
 		wint_by_year[[i]]["Discharge_ft3_day"] <- as.numeric(wint_by_year[[i]]$Discharge_cfs)*86400
 		wint_by_year[[i]]["Discharge_acft_day"] <- wint_by_year[[i]]$Discharge_ft3_day*(2.29568411e-5)
@@ -33,12 +33,12 @@ Split3Winter <- function(input){
 		stats[[i]] <- list()
 		stats[[i]][["Values"]] <-list()
 		stats[[i]][["Quantiles"]] <-list()
-		stats[[i]][["Values"]]["Total_Q_acfte6"] <- sum(wint_by_year[[i]][["Discharge_acfte6_day"]])
-		stats[[i]][["Quantiles"]][["Quantiles_acfte6"]] <-quantile(wint_by_year[[i]][["Discharge_acfte6_day"]], probs=c(0.05,0.1,0.2,0.25,0.5,0.75,0.9,0.95))
-		stats[[i]][["Values"]]["Mean_acfte6"] <- mean(wint_by_year[[i]][["Discharge_acfte6_day"]])
-		stats[[i]][["Values"]]["Median_acfte6"] <- median(wint_by_year[[i]][["Discharge_acfte6_day"]])
-		stats[[i]][["Values"]]["min_acfte6"] <- min(wint_by_year[[i]][["Discharge_acfte6_day"]])
-		stats[[i]][["Values"]]["max_acfte6"] <- max(wint_by_year[[i]][["Discharge_acfte6_day"]])
+		stats[[i]][["Values"]]["Total_Q_acfte6"] <- sum(wint_by_year[[i]][["Discharge_acfte6_day"]], na.rm=TRUE)
+		stats[[i]][["Quantiles"]][["Quantiles_acfte6"]] <-quantile(wint_by_year[[i]][["Discharge_acfte6_day"]], probs=c(0.05,0.1,0.2,0.25,0.5,0.75,0.9,0.95), na.rm=TRUE)
+		stats[[i]][["Values"]]["Mean_acfte6"] <- mean(wint_by_year[[i]][["Discharge_acfte6_day"]], na.rm=TRUE)
+		stats[[i]][["Values"]]["Median_acfte6"] <- median(wint_by_year[[i]][["Discharge_acfte6_day"]], na.rm=TRUE)
+		stats[[i]][["Values"]]["min_acfte6"] <- min(wint_by_year[[i]][["Discharge_acfte6_day"]], na.rm=TRUE)
+		stats[[i]][["Values"]]["max_acfte6"] <- max(wint_by_year[[i]][["Discharge_acfte6_day"]], na.rm=TRUE)
 		names(stats)[i] <- names(wint_by_year)[i]
 	}
 	total <- list(Data=wint_by_year, Stats=stats)
--- R/SplitWinterMonthly.R
+++ R/SplitWinterMonthly.R
@@ -13,9 +13,9 @@ SplitWinterMonthly <- function(input){
 #
 	if (missing(input))
 		stop("Input data is required.")
-	year <- as.numeric(format(input$raw$Date,"%Y"))
-	month <- as.numeric(format(input$raw$Date,"%m"))
-	day <- as.numeric(format(input$raw$Date,"%d"))
+	year <- as.numeric(format(input$Date,"%Y"))
+	month <- as.numeric(format(input$Date,"%m"))
+	day <- as.numeric(format(input$Date,"%d"))
 	years <- seq(min(year), max(year), by=1)
 	
 	wint_by_year <- list()
@@ -31,7 +31,7 @@ SplitWinterMonthly <- function(input){
 			} else {
 				year_select <- years[i]+1
 			}
-			wint_by_year[[i]][[n]] <- filter(input$raw, (month == months[n] & year == year_select))
+			wint_by_year[[i]][[n]] <- filter(input, (month == months[n] & year == year_select))
 			wint_by_year[[i]][[n]]["Discharge_ft3_day"] <- as.numeric(wint_by_year[[i]][[n]]$Discharge_cfs)*86400
 			wint_by_year[[i]][[n]]["Discharge_acft_day"] <- wint_by_year[[i]][[n]]$Discharge_ft3_day*(2.29568411e-5)
 			wint_by_year[[i]][[n]]["Discharge_acfte6_day"] <- wint_by_year[[i]][[n]]$Discharge_acft_day*1e-6
@@ -48,12 +48,12 @@ SplitWinterMonthly <- function(input){
 			stats[[i]][[n]] <-list()
 			stats[[i]][[n]][["Values"]] <-list()
 			stats[[i]][[n]][["Quantiles"]] <-list()
-			stats[[i]][[n]][["Values"]]["Total_Q_acfte6"] <- sum(wint_by_year[[i]][[n]][["Discharge_acfte6_day"]])
-			stats[[i]][[n]][["Quantiles"]][["Quantiles_acfte6"]] <-quantile(wint_by_year[[i]][[n]][["Discharge_acfte6_day"]], probs=c(0.05,0.1,0.2,0.25,0.5,0.75,0.9,0.95))
-			stats[[i]][[n]][["Values"]]["Mean_acfte6"] <- mean(wint_by_year[[i]][[n]][["Discharge_acfte6_day"]])
-			stats[[i]][[n]][["Values"]]["Median_acfte6"] <- median(wint_by_year[[i]][[n]][["Discharge_acfte6_day"]])
-			stats[[i]][[n]][["Values"]]["min_acfte6"] <- min(wint_by_year[[i]][[n]][["Discharge_acfte6_day"]])
-			stats[[i]][[n]][["Values"]]["max_acfte6"] <- max(wint_by_year[[i]][[n]][["Discharge_acfte6_day"]])
+			stats[[i]][[n]][["Values"]]["Total_Q_acfte6"] <- sum(wint_by_year[[i]][[n]][["Discharge_acfte6_day"]], na.rm=TRUE)
+			stats[[i]][[n]][["Quantiles"]][["Quantiles_acfte6"]] <-quantile(wint_by_year[[i]][[n]][["Discharge_acfte6_day"]], probs=c(0.05,0.1,0.2,0.25,0.5,0.75,0.9,0.95), na.rm=TRUE)
+			stats[[i]][[n]][["Values"]]["Mean_acfte6"] <- mean(wint_by_year[[i]][[n]][["Discharge_acfte6_day"]], na.rm=TRUE)
+			stats[[i]][[n]][["Values"]]["Median_acfte6"] <- median(wint_by_year[[i]][[n]][["Discharge_acfte6_day"]], na.rm=TRUE)
+			stats[[i]][[n]][["Values"]]["min_acfte6"] <- min(wint_by_year[[i]][[n]][["Discharge_acfte6_day"]], na.rm=TRUE)
+			stats[[i]][[n]][["Values"]]["max_acfte6"] <- max(wint_by_year[[i]][[n]][["Discharge_acfte6_day"]], na.rm=TRUE)
 			names(stats[[i]])[n] <- month_names[n]
 			names(stats)[i] <- names(wint_by_year)[i]
 	}
