ed98e5d26a0b6704c0d7dd0d5291754adca9950b
Tiffany_home <tnkocis@users.noreply.github.com>
2015-07-03 15:57:14 -0700

    updates

---
+++ R/hydrographTEST.R
@@ -0,0 +1,13 @@
+# TODO: Add comment
+# 
+# Author: tiffn_000
+###############################################################################
+
+
+plot(blah$All$Continuous, type="n", ylim=c(0,2e-4))
+lines(blah$All$`1960s`, col="red")
+lines(blah$All$`1970s`, col="orange")
+lines(blah$All$`1980s`, col="yellow")
+lines(blah$All$`1990s`, col="green")
+lines(blah$All$`2000s`, col="blue")
+lines(blah$All$`2010s`, col="purple")
