a348c4edf3daf2a6fbefe77429ad2d35e866ba14
Tiffany_home <tnkocis@users.noreply.github.com>
2015-07-16 21:48:04 -0700

    test

---
+++ R/test.R
@@ -0,0 +1,10 @@
+# TODO: Add comment
+# 
+# Author: tiffn_000
+###############################################################################
+
+
+write.csv(USGS11377100$Daysmax$df$X1DayMaxQ_maf, file="C:\\Users\\tiffn_000\\Desktop\\Data\\sac11377100_1daymax.csv")
+write.csv(USGS11377100$Daysmax$df$X3DayMaxQ_maf, file="C:\\Users\\tiffn_000\\Desktop\\Data\\sac11377100_3daymax.csv")
+write.csv(USGS11377100$Daysmax$df$X7DayMaxQ_maf, file="C:\\Users\\tiffn_000\\Desktop\\Data\\sac11377100_7daymax.csv")
+
